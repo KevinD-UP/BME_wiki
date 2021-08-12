e7ec2e780270046547062b9e19445a90e84b2d28
Kévin DANG <kevin.dang01@etu.univ-paris-diderot.fr>
2021-08-12 04:07:42 +0200

    work done

---
+++ script/script.js
@@ -0,0 +1,77 @@
+  //Get the button
+  let mybutton = document.getElementById("btn-back-to-top");
+
+  window.onscroll = function() {
+      scrollFunction()
+  };
+
+  function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          document.getElementById("navbar").style.background = "#8a1538";
+          mybutton.style.display = "block";
+      } else {
+
+          document.getElementById("navbar").style.background = "none";
+          mybutton.style.display = "none"
+      }
+  }
+  // When the user clicks on the button, scroll to the top of the document
+  mybutton.addEventListener("click", backToTop);
+
+  function backToTop() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+  }
+
+  var textWrapper = document.querySelector('.ml6');
+  textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+  anime.timeline({
+          loop: true
+      })
+      .add({
+          targets: '.ml6 .letter',
+          scale: [4, 1],
+          opacity: [0, 1],
+          translateZ: 0,
+          easing: "easeOutExpo",
+          duration: 950,
+          delay: (el, i) => 70 * i
+      }).add({
+          targets: '.ml6',
+          opacity: 0,
+          duration: 1000,
+          easing: "easeOutExpo",
+          delay: 1000
+      });
+
+  // Wrap every letter in a span
+  var textWrapper = document.querySelector('.ml14 .letters');
+  textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+  anime.timeline({
+          loop: true
+      })
+      .add({
+          targets: '.ml14 .line',
+          scaleX: [0, 1],
+          opacity: [0.5, 1],
+          easing: "easeInOutExpo",
+          duration: 900
+      }).add({
+          targets: '.ml14 .letter',
+          opacity: [0, 1],
+          translateX: [40, 0],
+          translateZ: 0,
+          scaleX: [0.3, 1],
+          easing: "easeOutExpo",
+          duration: 800,
+          offset: '-=600',
+          delay: (el, i) => 150 + 25 * i
+      }).add({
+          targets: '.ml14',
+          opacity: 0,
+          duration: 1000,
+          easing: "easeOutExpo",
+          delay: 2500
+      });
